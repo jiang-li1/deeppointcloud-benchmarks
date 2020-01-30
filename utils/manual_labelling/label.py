54c8a5830fc063be0f20125014ecb99dd27f2c29
Tristan Heywood <tristanheywood@hotmail.com>
2020-01-30 22:40:47 +0100

    experiment with open3d based labeller

--- utils/manual_labelling/label.py
+++ utils/manual_labelling/label.py
@@ -1,30 +1,84 @@
 import os.path as osp
 import sys
-ROOT = osp.join(osp.dirname(osp.realpath(__file__)), '..', '..')
+
+ROOT = osp.join(osp.dirname(osp.realpath(__file__)), "..", "..")
 sys.path.append(ROOT)
 
+import numpy as np
 import open3d as o3d
-
+import time
 from utils.custom_datasets.ahn_pointcloud import AHNPointCloud
+from utils.visualization.pcd_utils import colour_z_grey
 
+o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Debug)
 
-datapath = '/home/tristan/data/'
+datapath = "/home/tristan/deeppointcloud-benchmarks/data/AHNTilesDataset/raw/"
 
-cloud = '37EN2_11_section.laz'
+cloud = "37EN2_11_tiny2.laz"
 
 pcd = AHNPointCloud.from_cloud(datapath + cloud)
+
+global o3dpcd
 
 o3dpcd = o3d.geometry.PointCloud()
 o3dpcd.points = o3d.utility.Vector3dVector(pcd.pos)
 
+o3dpcd = colour_z_grey(o3dpcd)
 
+vis = o3d.visualization.VisualizerWithEditing()
+vis.create_window()
+vis.add_geometry(o3dpcd)
+
+
+def anim_cb(v: o3d.visualization.Visualizer):
+    global o3dpcd
+    pass
+    idx = v.get_selected_points()
+
+    if len(idx) > 0:
+        np.asarray(o3dpcd.colors)[idx] *= np.array([1, 0, 0])
+
+        # v.update_geometry(o3dpcd)
+        v.clear_geometries()
+        # v.remove_geometry(o3dpcd, False)
+        v.add_geometry(o3dpcd, False)
+
+        # v.update_renderer()
+        # print(np.asarray(o3dpcd.colors)[idx])
+        return True
+
+    return False
+
+
+vis.register_animation_callback(anim_cb)
+
+vis.run()
+vis.destroy_window()
+
+# print('starting vis loop')
+# start = time.time()
+# while time.time() - start < 10:
+#     vis.update_geometry(o3dpcd)
+#     vis.poll_events()
+#     vis.update_renderer()
+
+# print('vis loop ended')
+
+
+def demo_crop_geometry(pcd):
+    print("Demo for manual geometry cropping")
+    print("1) Press 'Y' twice to align geometry with negative direction of y-axis")
+    print("2) Press 'K' to lock screen and to switch to selection mode")
+    print("3) Drag for rectangle selection,")
+    print("   or use ctrl + left click for polygon selection")
+    print("4) Press 'C' to get a selected geometry and to save it")
+    print("5) Press 'F' to switch to freeview mode")
+    o3d.visualization.draw_geometries_with_editing([pcd])
 
 
 def pick_points(pcd):
     print("")
-    print(
-        "1) Please pick at least three correspondences using [shift + left click]"
-    )
+    print("1) Please pick at least three correspondences using [shift + left click]")
     print("   Press [shift + right click] to undo point picking")
     print("2) Afther picking points, press q for close the window")
     vis = o3d.visualization.VisualizerWithEditing()
--- utils/visualization/pcd_utils.py
+++ utils/visualization/pcd_utils.py
@@ -1,68 +1,69 @@
-
 import os.path as osp
 import sys
-ROOT = osp.join(osp.dirname(osp.realpath(__file__)), '..', '..')
+
+ROOT = osp.join(osp.dirname(osp.realpath(__file__)), "..", "..")
 sys.path.append(ROOT)
 
 import torch
-import numpy as np 
+import numpy as np
 import open3d as o3d
 
 from src.datasets.base_patch_dataset import PointCloud, ClassifiedPointCloud
 from utils.custom_datasets.file_pointcloud import FilePointCloud
 
-def pointcloud_to_o3d_pcd(cloud : PointCloud):
+
+def pointcloud_to_o3d_pcd(cloud: PointCloud):
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(cloud.pos.numpy())
     return pcd
+
 
 def file_pointcloud_to_o3d_pcd(cloud: FilePointCloud):
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(cloud.pos)
     return pcd
 
+
+def colour_z_grey(pcd: o3d.geometry.PointCloud):
+    pcd.paint_uniform_color([0.5] * 3)
+
+    bb: o3d.geometry.AxisAlignedBoundingBox = pcd.get_axis_aligned_bounding_box()
+    posZScale = (bb.get_max_bound()[2] - np.asarray(pcd.points)[:, 2]) / (bb.get_max_bound()[2] - bb.get_min_bound()[2])
+
+    colours = np.concatenate(
+        (np.expand_dims(posZScale, 1), np.expand_dims(posZScale, 1), np.expand_dims(posZScale, 1),), axis=1
+    )
+    np.asarray(pcd.colors)[:] = colours
+    return pcd
+
+
 def clas_pointcloud_to_z_coloured_o3d_pcd(cloud: PointCloud, classes: torch.tensor):
 
     pcd = pointcloud_to_z_grey_o3d_pcd(cloud)
 
-    classColours = np.array([
-        [1, 0, 0], 
-        [0, 1, 0], 
-        [0, 0, 1],
-        [1, 1, 0], 
-        [1, 0, 1], 
-        [0, 1, 1],
-        [0.5, 0.5, 0.5],
-    ])
+    classColours = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 0], [1, 0, 1], [0, 1, 1], [0.5, 0.5, 0.5],])
 
     np.asarray(pcd.colors)[:] *= classColours[classes.cpu().numpy()]
     return pcd
-    
+
 
 def pointcloud_to_z_grey_o3d_pcd(cloud: PointCloud) -> o3d.geometry.PointCloud:
     pcd = pointcloud_to_o3d_pcd(cloud)
 
-    posZScale = (cloud.maxPoint[2].item() - cloud.pos[:,2]) / (cloud.maxPoint[2].item() - cloud.minPoint[2].item())
+    posZScale = (cloud.maxPoint[2].item() - cloud.pos[:, 2]) / (cloud.maxPoint[2].item() - cloud.minPoint[2].item())
 
     colours = np.concatenate(
-        (
-            np.expand_dims(posZScale, 1),
-            np.expand_dims(posZScale, 1),
-            np.expand_dims(posZScale, 1),
-        ),
-        axis=1
+        (np.expand_dims(posZScale, 1), np.expand_dims(posZScale, 1), np.expand_dims(posZScale, 1),), axis=1
     )
 
-    pcd.paint_uniform_color([0.5]*3)
+    pcd.paint_uniform_color([0.5] * 3)
     np.asarray(pcd.colors)[:] = colours
     return pcd
 
+
 def rect_to_o3d_lineset(minXY, maxXY, minZ=0, maxZ=1, colour=[1, 0, 0]):
-    return block_to_o3d_lineset(
-        (*minXY, minZ),
-        (*maxXY, maxZ),
-        colour=colour,
-    )
+    return block_to_o3d_lineset((*minXY, minZ), (*maxXY, maxZ), colour=colour,)
+
 
 def block_to_o3d_lineset(minXYZ, maxXYZ, colour=[1, 0, 0]):
     pointsIdx = [
@@ -78,12 +79,7 @@ def block_to_o3d_lineset(minXYZ, maxXYZ, colour=[1, 0, 0]):
 
     points = []
     for corner in pointsIdx:
-        points.append(
-            [
-                (minXYZ if minOrMax == 0 else maxXYZ)[dim] 
-                for dim, minOrMax in enumerate(corner)
-            ]
-        )
+        points.append([(minXYZ if minOrMax == 0 else maxXYZ)[dim] for dim, minOrMax in enumerate(corner)])
 
     lines = [
         [0, 1],
@@ -100,16 +96,12 @@ def block_to_o3d_lineset(minXYZ, maxXYZ, colour=[1, 0, 0]):
         [3, 7],
     ]
     colors = [colour for i in range(len(lines))]
-    line_set = o3d.geometry.LineSet(
-        points=o3d.utility.Vector3dVector(points),
-        lines=o3d.utility.Vector2iVector(lines),
-    )
+    line_set = o3d.geometry.LineSet(points=o3d.utility.Vector3dVector(points), lines=o3d.utility.Vector2iVector(lines),)
     line_set.colors = o3d.utility.Vector3dVector(colors)
-    
+
     return line_set
 
 
 def visualize_pointcloud(cloud):
     pcd = pointcloud_to_o3d_pcd(cloud)
     o3d.visualization.draw_geometries([pcd])
-
