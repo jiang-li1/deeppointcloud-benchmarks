c147a45e69db01e94896cd551f2bfc7a5c9be998
Tristan Heywood <tristanheywood@hotmail.com>
2020-02-20 13:44:56 +0100

    add run file for dense randla on hpc

---
+++ run/randlad_hpc.sh
@@ -0,0 +1,1 @@
+python train.py data.dataroot=/scratch/theywood/ model_name=Randlanet_ahn_dense model_type=randlanet experiment_name=randla_dense data.patch_opt.patch_diam=50 data.patch_opt.context_dist=5 training.num_workers=8 training.resume=True training.cuda=True
